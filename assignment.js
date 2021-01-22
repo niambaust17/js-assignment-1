6c150167aa7c09f381ee204c2194df00cce9e4b5
unknown <niambaust17@gmail.com>
2021-01-22 15:13:11 +0600

    add some simple change

--- assignment.js
+++ assignment.js
@@ -1,75 +1,102 @@
+// 
+
 function kilometerToMeter(kilometer)
 {
-    if (kilometer < 0)
+    // if will check user give right or wrong input(like string, array and blank input)
+    if (kilometer <= 0 || typeof (kilometer) != "number")
     {
-        return "Please enter a positive number or zero(0) as input.";
-    } else
+        return "Please enter a positive number as input.";
+    }
+    else
     {
-        var meter = kilometer * 1000
-        return meter;
+        return kilometer * 1000;
     }
 }
+
+
+
 function budgetCalculator(watch, phone, laptop)
 {
     var watchPricePerUnit = 50;
     var phonePricePerUnit = 100;
     var laptopPricePerUnit = 500;
-    if (watch < 0 || phone < 0 || laptop < 0)
+    // if will check user give right or wrong input(like string, array and blank input)
+    if (watch <= 0 || phone <= 0 || laptop <= 0 || typeof (watch) != "number" || typeof (phone) != "number" || typeof (laptop) != "number")
     {
-        return "Please enter 3 positive numbers or zero(0) as input if you don't want to buy any specific product.";
-    } else
+        return "Please enter 3 positive numbers as input.";
+    }
+    else
     {
+        // if user input floating number parseInt will handle it and make it a integer number then calculate total budget
         var totalBudget =
-            watch * watchPricePerUnit +
-            phone * phonePricePerUnit +
-            laptop * laptopPricePerUnit;
+            parseInt(watch) * watchPricePerUnit +
+            parseInt(phone) * phonePricePerUnit +
+            parseInt(laptop) * laptopPricePerUnit;
         return totalBudget;
     }
 }
+
+
+
 function hotelCost(days)
 {
-    if (days <= 0)
+    if (days <= 0 || typeof (days) != "number")
     {
-        return "Please enter a positive value";
+        return "Please enter a positive value as input.";
     }
     else if (days <= 10)
     {
-        return days * 100;
+        // parseInt handle floating value 
+        return parseInt(days) * 100;
     }
     else if (days <= 20)
     {
+        // parseInt handle floating value 
         var firstSlotCost = 10 * 100;
         var secondSlotRemainingDay = days - 10;
-        var secondSlotCost = secondSlotRemainingDay * 80;
+        var secondSlotCost = parseInt(secondSlotRemainingDay) * 80;
         return firstSlotCost + secondSlotCost;
     }
     else
     {
+        // parseInt handle floating value 
         var firstSlotCost = 10 * 100;
         var secondSlotCost = 10 * 80;
         var finalSlotRemainingDay = days - 20;
-        var finalSlotCost = finalSlotRemainingDay * 50;
+        var finalSlotCost = parseInt(finalSlotRemainingDay) * 50;
         return firstSlotCost + secondSlotCost + finalSlotCost;
     }
 }
+
+
+
 function megaFriend(friendNames)
 {
-    var largestFriendName = friendNames[0];
-    for (var i = 0; i < friendNames.length; i++)
+    // if will check you input a empty array or another type of input without an array
+    if (friendNames.length == 0 || typeof (friendNames) != 'object')
     {
-        if (largestFriendName.length < friendNames[i].length)
-        {
-            largestFriendName = friendNames[i];
-        }
+        return "You enter a empty array please fill it with your friends name";
     }
-    return largestFriendName;
+    else
+    {
+        var largestFriendName = friendNames[0];
+        for (var i = 0; i < friendNames.length; i++)
+        {
+            if (largestFriendName.length < friendNames[i].length)
+            {
+                largestFriendName = friendNames[i];
+            }
+        }
+        return largestFriendName;
+    }
+
 }
 
-// var kilo = kilometerToMeter(6);
+// var kilo = kilometerToMeter(6.7);
 // console.log(kilo);
-// var budget = budgetCalculator(10, 0, 0);
+// var budget = budgetCalculator(10, 3.4, 5.6);
 // console.log(budget);
-// var cost = hotelCost(-10);
+// var cost = hotelCost(40.5);
 // console.log(cost);
-// var largestName = megaFriend(["abir", "akash roy", "rabea", "fultushi  ", "john", "hero", "mou"]);
+// var largestName = megaFriend(['abc', 'def', 'ghijk', 'lmno']);
 // console.log(largestName);
